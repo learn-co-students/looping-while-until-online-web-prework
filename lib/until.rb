8eaaf25ed7c2576d5252bf531a2e8ec0dc5f4d08
Kat B <katreesebarnes@gmail.com>
2019-02-27 21:12:55 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -5,6 +5,6 @@ def using_until
         levitation_force += 1
       
         
-    
+  end  
 end
 
--- lib/while.rb
+++ lib/while.rb
@@ -4,7 +4,7 @@ def using_while
 	levitation_force += 1 
 	puts "Wingardium Leviosa"
 	
-	#your code here
+	end  #your code here
 end
 
 
