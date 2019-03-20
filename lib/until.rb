68477d3b7b9849ada5422d79aae2713243326303
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-20 15:10:44 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,9 @@
+# Output "Wingardium Leviosa" 4 times, incrementing from 6
 def using_until
-        levitation_force = 6
-        #your code here
-    
+  levitation_force = 6
+	until levitation_force == 10
+	  puts "Wingardium Leviosa"
+	  levitation_force += 1
+	end
 end
 
