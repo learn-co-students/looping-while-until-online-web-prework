94550c410518913aada48a0b5356802a56eea025
Eric Bodden <ericbodden30@gmail.com>
2019-04-12 22:24:45 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,6 @@
-def using_until
-        levitation_force = 6
-        #your code here
-    
+def"puts out the phrase 'Wingardium Leviosa' until the levitation force is incremented to 10" do 
+	  looping_string = "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
+	  expect{ using_until }.to output(looping_string).to_stdout
+  end
 end
 
