bd48c8bbfae37e21c09e2b303d0a7b73907fe37d
johncban <johncban@gmail.com>
2019-04-14 17:48:05 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,9 @@
+require_relative('../lib/while.rb')
+
 def using_until
         levitation_force = 6
         #your code here
-    
+        until levitation_force == 10
+      		puts "Wingardium Leviosa"
+      		levitation_force +=1
 end
-
